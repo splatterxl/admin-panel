94ab1dfd75e4e99ec61f5707e313266ab7c935a1
splatterxl <splatterxl@outlook.ie>
2022-08-26 04:21:32 +0000

    fix logo being super tall on load
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, useBreakpointValue } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react"
 import Link from "next/link"
 import React from "react"
 import AuthStore from "../../stores/AuthStore"
@@ -21,22 +21,40 @@ export const Navbar: React.FC = () => {
       p={5}
       gap={{ md: 12, base: 3 }}
     >
-      <Box w={52} as={Link} href={Endpoints.HOME} passHref>
-        <a>
-          <Image
-            src={useBreakpointValue({
-              base: "/assets/patchcord/patchcord.png",
-              md: "/assets/patchcord/patchcord-big.png",
-            })}
-            alt="Patchcord logo"
-            userSelect="none"
-            width={{ md: 52, base: 12 }}
-            height="auto"
-            _light={{
-              filter: "invert(1)",
-            }}
-          />
-        </a>
+      <Box
+        w={{
+          md: 48,
+          base: 20,
+        }}
+      >
+        <Box
+          position={{
+            base: "absolute",
+            md: "initial",
+          }}
+          top={0}
+          left={0}
+          mt={{ base: 3, md: 0 }}
+          ml={{ base: 6, md: 0 }}
+        >
+          <Link href={Endpoints.HOME} passHref>
+            <a>
+              <Image
+                src="/assets/patchcord/patchcord-big.png"
+                alt="Patchcord logo"
+                userSelect="none"
+                width={{ md: 48, base: 48 }}
+                _light={{
+                  filter: "invert(1)",
+                }}
+                clipPath={{
+                  base: "polygon(0 0, 25% 0, 25% 100%, 0% 100%)",
+                  md: "none",
+                }}
+              />
+            </a>
+          </Link>
+        </Box>
       </Box>
       <Searchbar />
       <Flex
