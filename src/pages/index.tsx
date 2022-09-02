80d8f95875f0d2606e6264d115ee43a39dd335f4
splatterxl <splatterxl@outlook.ie>
2022-09-02 22:41:28 +0000

    i broke it smh
    Signed-off-by: GitHub <noreply@github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { Heading, Link, Text } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
 import type { NextPage } from "next"
-import NextLink from "next/link"
-import { Counts } from "../components/homepage/counts/Counts"
 import { Navbar } from "../components/layout/navbar/Navbar"
 import CurrentUserStore from "../stores/CurrentUserStore"
-import { Endpoints } from "../util/constants"
-import { greeting } from "../util/greeting"
 
 // TODO: homepage
 const Home: NextPage = () => {
