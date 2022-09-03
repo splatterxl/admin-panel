2b3ec3576830c59d4bf38c5122927e7b1b7ebb54
splatterxl <splatterxl@outlook.ie>
2022-09-03 14:45:21 +0100

    fix infinitely loading guilds

--- src/components/homepage/servers/InternalServer.tsx
+++ src/components/homepage/servers/InternalServer.tsx
@@ -1,11 +1,18 @@
-import { APIGuild, Routes } from "discord-api-types/v10"
-import useSWR from "swr"
+import { APIGuild } from "discord-api-types/v10"
+import React from "react"
 import http from "../../../util/http"
+import { PatchcordRoutes } from "../../../util/http"
 import { GuildIcon } from "../../guilds/GuildIcon"
 import { TableRow } from "../../layout/table/rows/TableRow"
 
 export const InternalServer: React.FC<{ id: string }> = ({ id }) => {
-  const { data: res, error } = useSWR(Routes.guild(id), http.get<APIGuild>)
+  const [data, setData] = React.useState<APIGuild>(null as any)
+
+  React.useEffect(() => {
+    http.get<APIGuild>(PatchcordRoutes.GUILD(id)).then(({ ok, data }) => {
+      if (ok) setData(data);
+    });
+  }, [])
 
   if (!res) return <TableRow>Loading... ({id})</TableRow>
 
