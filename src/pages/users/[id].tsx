344c6d3dc296df633913bae72a958e7e56769694
Splatterxl <splatterxl@outlook.ie>
2022-10-03 14:00:17 +0100

    use user cache for main user page
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -5,17 +5,16 @@ import React from "react"
 import { FullscreenSpinner } from "../../components/layout/FullscreenSpinner"
 import { Navbar } from "../../components/layout/navbar/Navbar"
 import { UserHeader } from "../../components/users/header/UserHeader"
+import UserCache from "../../stores/cache/UserCache"
 import FocusedUserStore from "../../stores/FocusedUserStore"
-import { PatchcordRoutes } from "../../util/constants"
-import http from "../../util/http"
 import { one } from "../../util/one"
-import { User } from "../../util/routes/types"
 
 export default function UserProfile() {
   const router = useRouter(),
     userId = one(router.query.id)!,
     // undefined: loading, null: not found
-    [user, setUser] = FocusedUserStore.useState()
+    [user, setUser] = FocusedUserStore.useState(),
+    getOrFetch = UserCache.useGetOrFetch()
 
   React.useEffect(() => {
     ;(async () => {
@@ -26,14 +25,18 @@ export default function UserProfile() {
 
       if (user && user.id !== userId) setUser(null)
 
-      const res = await http.get<User>(PatchcordRoutes.USER(userId))
-
-      if (!res.ok) {
+      try {
+        // FIXME: this has a TTL of as long as the user keeps the page open, we should ideally
+        // use the item in cache and update in an asynchronous operation, showing the user
+        // outdated data and updating as soon as we have the new info
+        //
+        // maybe we should invest in a websocket connection?
+        const res = await getOrFetch(userId)
+        setUser(res)
+      } catch {
         router.replace("/404", router.asPath)
         return
       }
-
-      setUser(res.data)
     })()
     // eslint-disable-next-line react-hooks/exhaustive-deps -- doesn't need add hook
   }, [userId])
